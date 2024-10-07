869e5156d34856d168280113ba0c779ac45c7fd2
ArtNov90 <art.novara@gmail.com>
2024-10-07 16:26:10 +0200

    création controller 2

--- src/main/java/com/digi/digihello/rest/HelloControleur.java
+++ src/main/java/com/digi/digihello/rest/HelloControleur.java
@@ -1,4 +1,4 @@
-package com.digi.restcontroller;
+package com.digi.digihello.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
