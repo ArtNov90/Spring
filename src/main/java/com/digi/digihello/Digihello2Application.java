3b18a08c05c285f3c63dc204930d7c78bcecdb42
ArtNov90 <art.novara@gmail.com>
2024-10-07 15:59:43 +0200

    first commit

---
+++ src/main/java/com/digi/digihello/Digihello2Application.java
@@ -0,0 +1,13 @@
+package com.digi.digihello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Digihello2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Digihello2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/digi/digihello/Digihello2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digi.digihello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Digihello2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
